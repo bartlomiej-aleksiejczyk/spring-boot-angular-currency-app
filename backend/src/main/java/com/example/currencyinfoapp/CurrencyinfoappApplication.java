b11ed29c0fec98eff02846dfeb269092eb9fbd36
Bartlomiej Aleksiejczyk <bartlomiej.aleksiejczyk@gmail.com>
2024-11-02 12:03:51 +0100

    chore: add initial frontend and backend setup

---
+++ backend/src/main/java/com/example/currencyinfoapp/CurrencyinfoappApplication.java
@@ -0,0 +1,13 @@
+package com.example.currencyinfoapp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CurrencyinfoappApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CurrencyinfoappApplication.class, args);
+	}
+
+}
---
+++ backend/src/test/java/com/example/currencyinfoapp/CurrencyinfoappApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.currencyinfoapp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CurrencyinfoappApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
