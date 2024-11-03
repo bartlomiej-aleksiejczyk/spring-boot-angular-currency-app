33c75c839e9e0668e714b052f9d0ed7a70e974b7
Bartlomiej Aleksiejczyk <bartlomiej.aleksiejczyk@gmail.com>
2024-11-04 00:09:06 +0100

    feat: add exchange rated view content

--- frontend/src/app/features/exchange-rate-view/exchange-rate-view.component.ts
+++ frontend/src/app/features/exchange-rate-view/exchange-rate-view.component.ts
@@ -1,12 +1,32 @@
-import { Component } from '@angular/core';
+import { Component, inject } from '@angular/core';
+import { CommonModule } from '@angular/common';
+
+import { ApiService } from '../../core/services/api.service';
+import { FormsModule } from '@angular/forms';
 
 @Component({
   selector: 'app-exchange-rate-view',
   standalone: true,
-  imports: [],
+  imports: [CommonModule, FormsModule],
   templateUrl: './exchange-rate-view.component.html',
-  styleUrl: './exchange-rate-view.component.scss'
+  styleUrl: './exchange-rate-view.component.scss',
 })
 export class ExchangeRateViewComponent {
+  private apiService = inject(ApiService);
+  currency = '';
+  name = '';
+  exchangeRate?: number;
 
+  getExchangeRate() {
+    this.apiService
+      .getCurrentCurrencyValue({ currency: this.currency, name: this.name })
+      .subscribe({
+        next: (response) => {
+          this.exchangeRate = response.value;
+        },
+        error: (error) => {
+          console.error('Error fetching exchange rate', error);
+        },
+      });
+  }
 }
