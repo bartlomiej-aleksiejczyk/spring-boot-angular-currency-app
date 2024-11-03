5264fbbe9782e5a30ce406a4eebf72127206e6dd
Bartlomiej Aleksiejczyk <bartlomiej.aleksiejczyk@gmail.com>
2024-11-04 00:08:44 +0100

    feat: add content to app queries view

--- frontend/src/app/features/app-queries-view/app-queries-view.component.ts
+++ frontend/src/app/features/app-queries-view/app-queries-view.component.ts
@@ -1,12 +1,27 @@
-import { Component } from '@angular/core';
+import { Component, OnInit, inject } from '@angular/core';
+import { ApiService } from '../../core/services/api.service';
+import { CommonModule } from '@angular/common';
+import { FormsModule } from '@angular/forms';
 
 @Component({
-  selector: 'app-app-queries-view',
+  selector: 'app-queries-view',
   standalone: true,
-  imports: [],
+  imports: [CommonModule, FormsModule],
   templateUrl: './app-queries-view.component.html',
-  styleUrl: './app-queries-view.component.scss'
+  styleUrl: './app-queries-view.component.scss',
 })
-export class AppQueriesViewComponent {
+export class AppQueriesViewComponent implements OnInit {
+  private apiService = inject(ApiService);
+  queries: any[] = [];
 
+  ngOnInit() {
+    this.apiService.getAllCurrencyQueries().subscribe({
+      next: (data) => {
+        this.queries = data;
+      },
+      error: (error) => {
+        console.error('Error fetching queries', error);
+      },
+    });
+  }
 }
