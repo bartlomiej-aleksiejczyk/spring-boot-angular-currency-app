4f9774f5986ce6ba1c21786d1ae7525e1c6b80a1
Bartlomiej Aleksiejczyk <bartlomiej.aleksiejczyk@gmail.com>
2024-11-04 00:04:48 +0100

    fix: change cors settings of api endpoints

--- backend/src/main/java/com/example/currencyinfoapp/exchangerate/ExchangeRateController.java
+++ backend/src/main/java/com/example/currencyinfoapp/exchangerate/ExchangeRateController.java
@@ -25,40 +25,49 @@ import com.example.currencyinfoapp.exchangerate.dtos.ExchangeRateResponseDTO;
 @RequiredArgsConstructor
 public class ExchangeRateController {
 
-    private final ExchangeRateService exchangeRateService;
+        private final ExchangeRateService exchangeRateService;
 
-    @PostMapping("/get-current-currency-value-command")
-    @ResponseStatus(HttpStatus.OK)
-    @Operation(summary = "Retrieves the current exchange rate of a specified currency",
-            description = "Post a currency code to receive its current exchange rate.",
-            responses = {@ApiResponse(responseCode = "200",
-                    description = "Successful retrieval of currency exchange rate",
-                    content = @Content(mediaType = "application/json",
-                            schema = @Schema(implementation = ExchangeRateResponseDTO.class),
-                            examples = {@ExampleObject(name = "Example",
-                                    value = "{\"value\": 1.1234}")})),
-                    @ApiResponse(responseCode = "400", description = "Invalid request parameters")})
-    public ExchangeRateResponseDTO getCurrentCurrencyValue(
-            @io.swagger.v3.oas.annotations.parameters.RequestBody(
-                    description = "Currency code and name of requester user", required = true,
-                    content = @Content(mediaType = "application/json",
-                            schema = @Schema(implementation = ExchangeRateRequestDTO.class),
-                            examples = {@ExampleObject(name = "Example 1",
-                                    value = "{\"currency\": \"USD\", \"name\": \"John Doe\"}"),
-                                    @ExampleObject(name = "Example 2",
-                                            value = "{\"currency\": \"EUR\", \"name\": \"Jane Doe\"}")})) @Valid @RequestBody ExchangeRateRequestDTO request) {
-        return exchangeRateService.getExchangeRate(request);
-    }
+        @CrossOrigin(origins = "*")
+        @PostMapping("/get-current-currency-value-command")
+        @ResponseStatus(HttpStatus.OK)
+        @Operation(summary = "Retrieves the current exchange rate of a specified currency",
+                        description = "Post a currency code to receive its current exchange rate.",
+                        responses = {@ApiResponse(responseCode = "200",
+                                        description = "Successful retrieval of currency exchange rate",
+                                        content = @Content(mediaType = "application/json",
+                                                        schema = @Schema(
+                                                                        implementation = ExchangeRateResponseDTO.class),
+                                                        examples = {@ExampleObject(name = "Example",
+                                                                        value = "{\"value\": 1.1234}")})),
+                                        @ApiResponse(responseCode = "400",
+                                                        description = "Invalid request parameters")})
+        public ExchangeRateResponseDTO getCurrentCurrencyValue(
+                        @io.swagger.v3.oas.annotations.parameters.RequestBody(
+                                        description = "Currency code and name of requester user",
+                                        required = true,
+                                        content = @Content(mediaType = "application/json",
+                                                        schema = @Schema(
+                                                                        implementation = ExchangeRateRequestDTO.class),
+                                                        examples = {@ExampleObject(
+                                                                        name = "Example 1",
+                                                                        value = "{\"currency\": \"USD\", \"name\": \"John Doe\"}"),
+                                                                        @ExampleObject(name = "Example 2",
+                                                                                        value = "{\"currency\": \"EUR\", \"name\": \"Jane Doe\"}")})) @Valid @RequestBody ExchangeRateRequestDTO request) {
+                return exchangeRateService.getExchangeRate(request);
+        }
 
 
-    @GetMapping("/requests")
-    @ResponseStatus(HttpStatus.OK)
-    @Operation(summary = "List all currency queries made",
-            description = "Fetch a list of all currency exchange queries.",
-            responses = {@ApiResponse(responseCode = "200", description = "List of all queries",
-                    content = @Content(mediaType = "application/json", array = @ArraySchema(
-                            schema = @Schema(implementation = ExchangeRateQueryDTO.class))))})
-    public List<ExchangeRateQueryDTO> getAllCurrencyQueries() {
-        return exchangeRateService.getAllQueries();
-    }
+        @CrossOrigin(origins = "*")
+        @GetMapping("/requests")
+        @ResponseStatus(HttpStatus.OK)
+        @Operation(summary = "List all currency queries made",
+                        description = "Fetch a list of all currency exchange queries.",
+                        responses = {@ApiResponse(responseCode = "200",
+                                        description = "List of all queries",
+                                        content = @Content(mediaType = "application/json",
+                                                        array = @ArraySchema(schema = @Schema(
+                                                                        implementation = ExchangeRateQueryDTO.class))))})
+        public List<ExchangeRateQueryDTO> getAllCurrencyQueries() {
+                return exchangeRateService.getAllQueries();
+        }
 }
